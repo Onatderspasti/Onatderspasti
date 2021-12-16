7c11335e7147ff0a46dc2636de80d7af9ae0519b
Osama bin Laden <96242155+Onatderspasti@users.noreply.github.com>
2021-12-16 14:13:13 +0100

    Create Richard.py
    Ist cool

---
+++ Richard.py
@@ -0,0 +1,11 @@
+
+i=3while i > 0:
+print(i)
+i = i- 1
+if i == 0:
+break
+x = "antwort"
+x = (input("Bundespresident der Schweiz: "))if x == "Guy Parmelin":
+print("Korrekt")else:
+print("Falsch")
+
